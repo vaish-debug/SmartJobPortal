c6d0eadb9ea07232b8f5d2482e44b10784a31a22
Sampurna07 <sampurnaadike@gmail.com>
2026-06-17 22:52:21 +0530

    Update ApplicationRepository.java

--- src/main/java/jobportal/repository/ApplicationRepository.java
+++ src/main/java/jobportal/repository/ApplicationRepository.java
@@ -1,7 +1,5 @@
 package jobportal.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import jobportal.entity.Application;
-
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
 }
