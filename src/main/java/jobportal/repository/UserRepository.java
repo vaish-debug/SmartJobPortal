c199eb1776f21857d24c3832b1c77bc0a8a98a50
Sampurna07 <sampurnaadike@gmail.com>
2026-06-17 22:53:16 +0530

    Update UserRepository.java

--- src/main/java/jobportal/repository/UserRepository.java
+++ src/main/java/jobportal/repository/UserRepository.java
@@ -1,15 +1,10 @@
 package jobportal.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import jobportal.entity.User;
-
 public interface UserRepository
 extends JpaRepository<User, Integer> {
-
 User findByEmailAndPassword(
         String email,
         String password);
-
 User findByEmail(String email);
-
 }
