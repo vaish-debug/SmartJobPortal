f61742659dc52a21950137b091c3a9858249503b
Sampurna07 <sampurnaadike@gmail.com>
2026-06-17 22:39:00 +0530

    Remove unnecessary whitespace in HomeController

--- src/main/java/jobportal/controller/HomeController.java
+++ src/main/java/jobportal/controller/HomeController.java
@@ -1,11 +1,8 @@
 package jobportal.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class HomeController {
-
     @GetMapping("/")
     public String home() {
         return "index";
