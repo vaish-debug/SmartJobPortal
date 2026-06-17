b82986beeb4b64d336f79a389b9421bfe1fa7f48
Sampurna07 <sampurnaadike@gmail.com>
2026-06-17 22:46:35 +0530

    Refactor Application class by removing whitespace

--- src/main/java/jobportal/entity/Application.java
+++ src/main/java/jobportal/entity/Application.java
@@ -1,11 +1,8 @@
 package jobportal.entity;
-
 import jakarta.persistence.*;
-
 @Entity
 @Table(name = "applications")
 public class Application {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -13,29 +10,22 @@ public class Application {
     private Integer userId;
     private Integer jobId;
     private String application;
-
     public Application() {}
-
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
-
     public Integer getUserId() {
         return userId;
     }
-
     public void setUserId(Integer userId) {
         this.userId = userId;
     }
-
     public Integer getJobId() {
         return jobId;
     }
-
     public void setJobId(Integer jobId) {
         this.jobId = jobId;
     }
@@ -43,14 +33,12 @@ public class Application {
     public String getResumeFileName() {
     return resumeFileName;
 }
-
 public void setResumeFileName(String resumeFileName) {
     this.resumeFileName = resumeFileName;
 }
 public String getStatus() {
     return status;
 }
-
 public void setStatus(String status) {
     this.status = status;
 }
