895df8cce32e27c21fa4ff58ef606751b83a3f33
Sampurna07 <sampurnaadike@gmail.com>
2026-06-17 23:33:37 +0530

    Remove unnecessary whitespace and fix formatting

--- src/test/java/jobportal/JobportalApplicationTests.java
+++ src/test/java/jobportal/JobportalApplicationTests.java
@@ -1,13 +1,9 @@
 package jobportal;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class JobportalApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
