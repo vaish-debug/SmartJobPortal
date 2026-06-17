6f0fadc5f28903651813e8117478b93cba331fc3
Sampurna07 <sampurnaadike@gmail.com>
2026-06-17 22:45:29 +0530

    Update Admin.java

--- src/main/java/jobportal/entity/Admin.java
+++ src/main/java/jobportal/entity/Admin.java
@@ -1,40 +1,29 @@
 package jobportal.entity;
-
 import jakarta.persistence.*;
-
 @Entity
 @Table(name="admins")
 public class Admin {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     private String username;
     private String password;
-
     public Admin() {}
-
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
-
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
