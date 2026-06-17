dd8ba0349a7dba52bfd5d9328410cd290cdbbc22
Sampurna07 <sampurnaadike@gmail.com>
2026-06-17 22:52:52 +0530

    Remove unnecessary blank lines in SavedJobRepository

--- src/main/java/jobportal/repository/SavedJobRepository.java
+++ src/main/java/jobportal/repository/SavedJobRepository.java
@@ -1,8 +1,6 @@
 package jobportal.repository;
-
 import jobportal.entity.SavedJob;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface SavedJobRepository
         extends JpaRepository<SavedJob,Integer> {
 }
